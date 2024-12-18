90e57ed46a5e2c4069055e2681854ef546ecd6f7
yz <yunjae00734@gmail.com>
2024-12-19 00:30:04 +0900

    fixed untracked files

--- cmake_cmb/Ninja_clang-cl/Debug/cmake-build.bat
+++
@@ -1,4 +0,0 @@
-﻿@echo off
-echo Building D:\devs\projects\cmb
-cmake --build D:\devs\projects\cmb\target\Debug
-pause
--- cmake_cmb/Ninja_clang-cl/Debug/cmake-run.bat
+++
@@ -1,4 +0,0 @@
-﻿@echo off
-echo Running D:\devs\projects\cmb
-cd /d "D:\devs\projects\cmb\out\Debug/bin"
-start cmb.exe
